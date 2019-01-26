7bf5bcddee780b3e33b1b469797e90491c92a0e8
roboriaan <roboriaan@gmail.com>
2019-01-26 20:48:22 +0200

    First commit
    First!

---
+++ BlazorServiceWorker.Client/wwwroot/js/main.js
@@ -0,0 +1,9 @@
+﻿// Make sure sw are supported
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker
+            .register('../sw_cached_site.js')
+            .then(reg => console.log('Service Worker: Registered (Pages)'))
+            .catch(err => console.log(`Service Worker: Error: ${err}`));
+    });
+}
---
+++ BlazorServiceWorker.Server/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace BlazorServiceWorker.Server
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseConfiguration(new ConfigurationBuilder()
+                    .AddCommandLine(args)
+                    .Build())
+                .UseStartup<Startup>()
+                .UseUrls("https://*:5001")
+                .Build();
+    }
+}
